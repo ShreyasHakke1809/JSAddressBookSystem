e257ed0d8981b34a333922fc8fda48391ac09b16
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-09-07 16:24:37 +0530

    Adding regex to address book system program

--- AddresBook.js
+++ AddresBook.js
@@ -1,7 +1,7 @@
 console.log("Welcome to AddressBook program using JavaScript")
 
 //Importing the neccessary module
-let prompt = require('prompt-sync')();
+let prompt = require("prompt-sync")();
 
 //Created contact class
 class Contact {
@@ -47,16 +47,46 @@ function AddContact(firstName, lastName, address, city, state, zipCode, phoneNum
 //Function to get the details of the contact from the user
 function getContactDetails() {
     try {
+        //Regex patterns for validating contact details(UC2)
+        let namePattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
+        let addressPattern = new RegExp('^[A-Za-z]{4,}$');
+        let zipCodePattern = new RegExp('^[1-9]{1}[0-9]{2}[ ]?[0-9]{3}$');
+        let phoneNumPattern = new RegExp('^[+]?91[ ]?[1-9]{1}[0-9]{9}$');
+        let emailIdPattern = new RegExp('^[a-zA-Z0-9]{3,}([._+-][0-9a-zA-Z]{2,})*@[0-9a-zA-Z]+[.]?([a-zA-Z]{2,4})+[.]?([a-zA-Z]{2,3})*$');
+        //Validating first name(UC2)
         let firstName = prompt('Enter Your FirstName : ');
+        if (!namePattern.test(firstName))
+            throw 'First name should have minimum 3 characters';
+        //Validating last name(UC2)
         let lastName = prompt('Enter Your LastName : ');
+        if (!namePattern.test(lastName))
+            throw 'First name should have minimum 3 characters';
+        //Validating address(UC2)
         let address = prompt('Enter Your Address : ');
+        if (!addressPattern.test(address))
+            throw 'Address should have minimum 4 characters';
+        //Validating city(UC2)
         let city = prompt('Enter Your City Name: ');
+        if (!addressPattern.test(city))
+            throw 'City should have minimum 4 characters';
+        //Validating states(UC2)
         let state = prompt('Enter Your State Name : ');
+        if (!addressPattern.test(state))
+            throw 'State should have minimum 4 characters';
+        //Validating zip code(UC2)
         let zipCode = parseInt(prompt('Enter Your Zip Code : '));
+        if (!zipCodePattern.test(zipCode))
+            throw 'Zipcode is not valid';
+        //Validating phone number(UC2)
         let phoneNumber = parseInt(prompt('Enter Your Phone Number : '));
+        if (!phoneNumPattern.test(phoneNumber))
+            throw 'Phone number is not valid';
+        //Validating email id(UC2)
         let emailId = prompt('Enter Your Email Id : ');
+        if (!emailIdPattern.test(emailId))
+            throw 'Email id is not valid';
         let contactDetails = AddContact(firstName, lastName, address, city, state, zipCode, phoneNumber, emailId);
-        console.log("\nDetails Of Contacts");
+        console.log("\nContact Details: ");
         console.log(contactDetails.toString());
     } catch (e) {
         console.error(e);
